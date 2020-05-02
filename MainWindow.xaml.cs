45e6a9903e767634fcd265d0aa560716e9cea962
hernikplays <hernik27@gmail.com>
2020-05-02 12:13:40 +0200

    Basic project

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace OcarinaPlayer
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private WaveOutEvent player = new WaveOutEvent();
+        private string file = string.Empty;
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Audio File|*.mp3;";
+            if (open.ShowDialog() == true)
+            {
+                MessageBox.Show(open.FileName);
+
+                file = open.FileName;
+
+            }
+            else
+            {
+                MessageBox.Show("An Error occured");
+                return;
+            }
+        }
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if(file == string.Empty)
+            {
+                 MessageBox.Show("You need to open a file first");
+                return;
+            }
+            if(player.PlaybackState == PlaybackState.Playing)
+            {
+                return;
+            }
+            WaveStream mainOutputStream = new Mp3FileReader(file);
+            WaveChannel32 volumeStream = new WaveChannel32(mainOutputStream);
+
+
+            player.Init(volumeStream);
+
+            player.Play();
+            playBtn.Source = new BitmapImage(new Uri("assets/img/pause.png", UriKind.Relative));
+        }
+    }
+}
