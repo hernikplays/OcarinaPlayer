391d828cb914bc4e51055a6564fec36bda47f372
hernikplays <hernik27@gmail.com>
2020-05-12 14:46:11 +0200

    added multiple select, play/pause/skip

--- src/cz/hernikplays/Ocarina/Controller.java
+++ src/cz/hernikplays/Ocarina/Controller.java
@@ -1,5 +1,9 @@
 package cz.hernikplays.Ocarina;
 
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -13,14 +17,19 @@ import net.arikia.dev.drpc.DiscordRPC;
 import net.arikia.dev.drpc.DiscordRichPresence;
 
 
+
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Controller {
     private Stage myStage;
     MediaPlayer mediaPlayer;
     FileChooser playFile = new FileChooser();
-    File selected;
+    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Music files (*.mp3)","*.mp3");
+    int fileIndex = 0;
+
+    List<File> selected;
 
     DiscordEventHandlers handlers;
 
@@ -28,6 +37,8 @@ public class Controller {
         DiscordRPC.discordInitialize("690238946378121296", handlers, true);
         DiscordRichPresence rich = new DiscordRichPresence.Builder("...").setDetails("Not listening to anything.").setBigImage("rpcon", "Ocarina Music Player").build();
         DiscordRPC.discordUpdatePresence(rich);
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Music files (*.mp3)","*.mp3");
+        playFile.getExtensionFilters().add(extFilter);
     }
 
     public void setStage(Stage stage) {
@@ -35,24 +46,56 @@ public class Controller {
     }
 
     public void openFile(ActionEvent event){
-        selected = playFile.showOpenDialog(myStage);
+        selected = playFile.showOpenMultipleDialog(myStage);
     }
 
     @FXML
-    public void play(ActionEvent event) {
+    public void play(ActionEvent event) throws InvalidDataException, IOException, UnsupportedTagException {
+
         Alert nofile = new Alert(Alert.AlertType.ERROR, "No file selected", ButtonType.OK);
         if(selected == null){
             nofile.show();
             return;
         }
-        Media hit = new Media(selected.toURI().toString());
+        if(mediaPlayer.getStatus() == MediaPlayer.Status.PAUSED){
+            mediaPlayer.play();
+        }
+        else if(mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING){
+            mediaPlayer.pause();
+        }
+        Media hit = new Media(selected.get(fileIndex).toURI().toString());
         mediaPlayer = new MediaPlayer(hit);
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         mediaPlayer.play();
 
+        Mp3File nowPlaying = new Mp3File(selected.get(fileIndex));
+        if(nowPlaying.hasId3v2Tag()){
+            ID3v2 tag = nowPlaying.getId3v2Tag();
+            String track = tag.getTitle();
+            String artist = tag.getArtist();
 
-
+            DiscordRPC.discordUpdatePresence(new DiscordRichPresence.Builder("by "+artist).setDetails("Listening to "+track).setBigImage("rpcon", "Ocarina Music Player").build());
+        }
         /*MediaView mediaView = new MediaView(mediaPlayer);
         ((Group)scene.getRoot()).getChildren().add(mediaView);*/
+    }
+
+    public void skip (ActionEvent event) throws InvalidDataException, IOException, UnsupportedTagException {
+        if(selected == null){
+            System.out.println("Selected is null");
+            return;
+        }
+        Alert eop = new Alert(Alert.AlertType.ERROR, "End of playlist", ButtonType.OK);
+        fileIndex++;
+        if(fileIndex == selected.size()){
+         eop.showAndWait();
+         fileIndex=0;
+         return;
+        }
+        if(mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING){
+            mediaPlayer.stop();
+        }
+
+        play(event);
     }
 }
