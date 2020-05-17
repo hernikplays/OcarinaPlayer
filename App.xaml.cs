69ef29a0935c98c1ea652e7ba867277cd6b39e48
hernikplays <hernik27@gmail.com>
2020-05-17 11:06:19 +0200

    added missing files

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+namespace OcarinaPlayer
+{
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+		
+	}
+}
