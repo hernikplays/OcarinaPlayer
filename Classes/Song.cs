988412c993e3dcd3695a487bfbd941fd64b4dc0b
Hernik <hernik27@gmail.com>
2021-01-02 13:52:15 +0100

    Added "Adding songs from music folder to c# list"
    Updates will from now on be posted on https://github.com/hernikplays/OcarinaPlayer/discussions/9#discussion-1236766

---
+++ Classes/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocarina.Classes
+{
+    public class Song
+    {
+        public string Name { get;set; }
+        public string Album { get; set; }
+        public string Author { get; set; }
+        public uint Position {get; set; }
+        public string Path { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CSCore.Codecs;
+using Ocarina.Classes;
+using Path = System.IO.Path;
 
 
 namespace Ocarina
@@ -21,6 +26,7 @@ namespace Ocarina
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Song> SongsInMusicFolder = new List<Song>();
         public MainWindow()
         {
             InitializeComponent();
@@ -28,8 +34,41 @@ namespace Ocarina
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            var user = System.DirectoryServices.AccountManagement.UserPrincipal.Current.DisplayName;
-            HiBlock.Text = "Hello, " + user;
+            /*var user = System.DirectoryServices.AccountManagement.UserPrincipal.Current.DisplayName;
+            HiBlock.Text = "Hello, " + user;*/
+
+            //* Find music folder and put songs into a list
+            var folderList = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            if (folderList.Length > 0)
+            {
+                foreach (var s in folderList)
+                {
+                    var typeArray = CodecFactory.SupportedFilesFilterEn.Replace("Supported Files|", "").Split(';');
+                    var ret = true;
+                    foreach (var type in typeArray)
+                    {
+                        if (s.EndsWith(type.Replace("*", "")))
+                        {
+                            ret = false;
+                        }
+                    }
+                    if (ret == false)
+                    {
+                        var tag = TagLib.File.Create(s);
+                        var song = new Song();
+                        if (string.IsNullOrEmpty(tag.Tag.Title)) song.Name = Path.GetFileName(s);
+                        else song.Name = tag.Tag.Title;
+                        if (string.IsNullOrEmpty(tag.Tag.Album)) song.Album = "Unknown";
+                        else song.Album = tag.Tag.Album;
+                        if (string.IsNullOrEmpty(tag.Tag.FirstPerformer)) song.Author = "Unknown";
+                        else song.Author = tag.Tag.FirstPerformer;
+                        song.Position = tag.Tag.Track;
+                        song.Path = s;
+                        SongsInMusicFolder.Add(song);
+                    }
+                }
+            }
         }
+
     }
 }
